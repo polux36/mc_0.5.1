269c3cffc28e18ffb95c46a784d1c3e231c333e0
Boris Pérez <polux27@gmail.com>
2015-11-10 21:34:54 -0500

    Se modifican los archivos de internacionalización: se añade la traducción en español

--- src/minicopier/gui/Credits.java
+++ src/minicopier/gui/Credits.java
@@ -135,18 +135,8 @@ public class Credits extends JDialog {
 		StringBuffer license = new StringBuffer();
 		
 		license.append(Language.get("Credits.License.Intro")+"\n\n"); 
-		license.append("Copyright (C) Adrian Courreges\n\n");
-		license.append("This program is free software; you can redistribute it and/or modify " +
-				"it under the terms of the GNU General Public License as published by " +
-				"the Free Software Foundation; either version 2 of the License, or " +
-				" (at your option) any later version.\n\n" +
-				"This program is distributed in the hope that it will be useful, " +
-				"but WITHOUT ANY WARRANTY; without even the implied warranty of " +
-				"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the " +
-				"GNU General Public License for more details.\n\n" +
-				"You should have received a copy of the GNU General Public License " +
-				"along with this program; see the file COPYING. If not, write to the " +
-				"Free Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.");
+		license.append("Credits.Copyrigth.To");
+		license.append("Credits.License");
 		
 		return license.toString();
 		
